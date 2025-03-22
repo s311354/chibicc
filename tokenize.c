76bbe6851ef79e429bbdc1cb706adb6954309c90
Louisliu <qazqazqaz850@gmail.com>
2025-03-22 23:05:42 +0800

    ensure memory-safe comparisons

--- tokenize.c
+++ tokenize.c
@@ -77,7 +77,7 @@ void warn_tok(Token *tok, char *fmt, ...) {
 
 // Consumes the current token if it matches `op`.
 bool equal(Token *tok, char *op) {
-  return memcmp(tok->loc, op, tok->len) == 0 && op[tok->len] == '\0';
+  return strlen(op) == tok->len && strncmp(tok->loc, op, tok->len) == 0;
 }
 
 // Ensure that the current token is `op`.
